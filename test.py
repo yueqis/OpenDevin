59d0d51bc7190aecd7097a51fea229130106c717
yueqis <141804823+yueqis@users.noreply.github.com>
2024-09-27 12:25:11 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,49 @@
+RootWebArea 'Projects · Dashboard · GitLab', focused
+	[51] banner ''
+		[58] link 'Dashboard', clickable
+			[59] image ''
+		[62] list ''
+			[63] listitem ''
+				[64] button '', clickable, hasPopup='menu', expanded=False
+	[243] main ''
+		[246] heading 'Projects'
+		[248] link 'New project', clickable
+			StaticText 'New project'
+		[256] list ''
+			[257] listitem ''
+				[258] link 'Yours 14', clickable
+					StaticText '14'
+			[260] listitem ''
+				[261] link 'Starred 3', clickable
+					StaticText '3'
+			[263] listitem ''
+				[264] link 'Explore', clickable
+			[265] listitem ''
+				[266] link 'Topics', clickable
+		[268] Section ''
+			[269] searchbox 'Filter by name', clickable
+			[273] button 'Name', clickable
+				StaticText 'Name'
+				[275] image ''
+		[305] list ''
+			[306] listitem ''
+				[307] link 'All', clickable
+			[308] listitem ''
+				[309] link 'Personal', clickable
+		[312] listitem ''
+			[314] link 'A', clickable
+				StaticText 'A'
+			[319] heading 'Byte Blaze / a11y-syntax-highlighting'
+				[320] link 'Byte Blaze / a11y-syntax-highlighting', clickable
+					StaticText 'Byte Blaze /'
+					StaticText 'a11y-syntax-highlighting'
+			[324] image ''
+			StaticText 'Owner'
+			[327] paragraph ''
+				StaticText '💄'
+				StaticText 'Accessible light and dark syntax highlighting themes'
+			[338] link '1', clickable
+				[339] image ''
+			[340] link '0', clickable
+				[341] image ''
+		
